ed16e09e60b98848aa51e203d33da6fbc2cc3458
rogert100x <55651032+rogert100x@users.noreply.github.com>
2020-09-21 17:44:46 +0200

    Delete test reverse name.py

--- test reverse name.py
+++
@@ -1,6 +0,0 @@
-print('This is your name backwards with no spaces:- ',end='')
-revname=''
-for f in reversed("roger thomas jenkins"):
-    if f!=' ':
-        revname=revname+f
-print(revname,' and \n is ',len(revname),'characters long')
